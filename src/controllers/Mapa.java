e24ba683e403fc164784bd3550d2d5c17c7e6f91
David Banegas Lazo <abanegasl@est.ups.edu.ec>
2025-07-08 19:57:07 -0500

    Trabajo en clase

--- src/controllers/Mapa.java
+++ src/controllers/Mapa.java
@@ -1,9 +1,101 @@
 package controllers;
-
+import java.util.Map;
+import java.util.TreeMap;
 import java.util.HashMap;
+import models.Empleado;
+
 
 public class Mapa {
     public Mapa() {
-        System.out.println("Mapa Class");
+        System.out.println("***********Mapa Class***********");
+    }
+    public void runHashMap() {
+        System.out.println("----------------------------Ejemplo de HashMap------------------------------");
+        Map<String, String>mapa = new HashMap();
+        mapa.put("001", "Celular"); 
+        mapa.put("002", "Laptop"); 
+        mapa.put("003", "Celular");  
+        mapa.put("003", "PC");
+
+        System.out.println(mapa);
+        System.out.println(mapa.keySet());
+
+        for (String key : mapa.keySet()) {
+            System.out.println(" Valor: " + mapa.get(key));
+        }
+        System.out.println("001");
+        System.out.println("005");
+     }
+
+    public void runLinkedHashMap() {
+        // Crear un mapa con clave numeros y vaalores nombre de personas solo nombre
+        //iteger-string
+        //Agregar 5 elementos e imprimir todos los VALORES
+        System.out.println("---------------------Ejemplo de LinkedHashMap----------------------");
+        Map<Integer, String> linkedMap = new java.util.LinkedHashMap<>();
+        linkedMap.put(1, "Juan");
+        linkedMap.put(2, "Maria");
+        linkedMap.put(3, "Pedro");
+        linkedMap.put(4, "Ana");
+        linkedMap.put(5, "Luis");
+        // Imprimir todos los valores
+        for (String value : linkedMap.values()) {
+            System.out.println("Valor: " + value);
+        }
+    }
+    public void runTreeMap() {
+        System.out.println("------------------------------Ejemplo de TreeMap------------------------------");
+        Map<Integer, String> items = new TreeMap();
+            items.put(3, "carro");
+            items.put(6, "bicicleta");
+            items.put(1, "moto");
+            
+            // IMPRIMIR VALORES
+            for (String value : items.values()) {
+                System.out.println("Valor: " + value);
+            }
+    }
+    public void runHashMapObj(){
+        System.out.println("----------------------Ejemplo de HashMap con objetos-----------------------");
+        Map<Empleado, Integer> empleados = new HashMap();
+
+        empleados.put(new Empleado(1, "Juan", "Dev"), 1200);
+        empleados.put(new Empleado(2, "Juan", "Dev"), 1500);
+        empleados.put(new Empleado(3, "Pedro", "Dev"), 2000);
+        empleados.put(new Empleado(1, "Juan", "Dev"), 2500);
+        empleados.put(new Empleado(1, "Diego", "Dev"), 3000);
+        //sobreescribimos el iquals y el Hashcode para comparar solo el id y el nombre 
+        // solo deberia quedar 4 empleados 1 pedro 2 juan y 1 diego
+        System.out.println("Empleados y sus salarios:");
+        for (Map.Entry<Empleado, Integer> entry : empleados.entrySet()) {
+            Empleado empleado = entry.getKey();
+            Integer salario = entry.getValue();
+            System.out.println(empleado + " - Salario: " + salario);
+        }
+    }
+    public void runTreemapObj(){
+        System.out.println("----------------------Ejemplo de TreeMap con objetos-----------------------");
+        Map<Empleado, Integer> empleados = new TreeMap();
+
+        empleados.put(new Empleado(1, "Juan", "Dev"), 1200);
+        empleados.put(new Empleado(2, "Juan", "Dev"), 1500);
+        empleados.put(new Empleado(3, "Pedro", "Dev"), 2000);
+        empleados.put(new Empleado(1, "Diego", "Dev"), 1200);
+        empleados.put(new Empleado(1, "Juan", "Senior"), 3000);
+
+
+
+        //comparamos si los numeros son iguales, si son iguales comparamos por nombre
+        //utilizamos el compareto de la clase empleado
+        //no necesito crear mas empleados
+        //es necesario que la clase empleado implemente la interfaz comparable
+        System.out.println("Empleados y sus salarios:");
+        for (Map.Entry<Empleado, Integer> entry : empleados.entrySet()) {
+            Empleado empleado = entry.getKey();
+            Integer salario = entry.getValue();
+            System.out.println(empleado + " - Salario: " + salario);
+        }
+
+
     }
 }
--- src/models/Empleado.java
+++ src/models/Empleado.java
@@ -1,6 +1,6 @@
 package models;
 
-public class Empleado {
+public class Empleado implements Comparable<Empleado>{
     private int id;
     private String name;
     private String position;
@@ -26,5 +26,31 @@ public class Empleado {
     @Override
     public String toString() {
         return "ID: " + id + ", Name: " + name + ", Position: " + position;
+    }
+    //sobreescribimos el iquals y el Hashcode para comparar solo el id y el nombre 
+    // solo deberia quedar 4 empleados 1 pedro 2 juan y 1 diego
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof Empleado)) return false;
+        Empleado other = (Empleado) obj;
+        return this.id == other.id && this.name.equals(other.name);
+    }
+    @Override
+    public int hashCode() {
+        int result = Integer.hashCode(id);
+        result = 31 * result + name.hashCode();
+        return result;
+    }
+    //En caso de que el id sea el mismo, se compara por nombre
+    // si el id es diferente se compara por id
+    @Override
+    public int compareTo(Empleado emp) {
+        int comparacionId = Integer.compare(id, emp.id);
+        if (comparacionId != 0) {
+            return comparacionId;
+        }
+        // Si los IDs son iguales, comparamos por nombre
+        return name.compareTo(emp.name);
     }
 }
